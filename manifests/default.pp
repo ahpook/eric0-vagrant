9ffde5f5f9b66f1e13151a515b91f7c96f8da8e4
Eric Sorenson <eric.sorenson@puppetlabs.com>
2014-11-11 14:18:07 -0800

    iptables tweaks, to support ubuntu and permit higgs installer

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,22 +1,36 @@
 
-# life without these is not worth living
-package { ['vim-enhanced','git']:
-  ensure => latest,
-}
+case $osfamily {
 
-# set up inbound 8140
-file { '/etc/sysconfig/iptables':
-  ensure  => present,
-  content => file('/tmp/vagrant-puppet/manifests/iptables'),
-  notify  => Service['iptables'],
-}
+  'RedHat': {
+    # life without these is not worth living
+    package { ['vim-enhanced','git']:
+      ensure => latest,
+    }
 
-service { 'iptables':
-  ensure => running,
+    # set up inbound 8140
+    file { '/etc/sysconfig/iptables':
+      ensure  => present,
+      content => file('/tmp/vagrant-puppet-2/manifests/iptables'),
+      notify  => Service['iptables'],
+    }
+
+    service { 'iptables':
+      ensure => running,
+    }
+
+  }
+  'Debian': {
+    # life without these is not worth living
+    package { ['vim','vim-puppet','tmux']:
+      ensure => latest,
+    }
+  }
+
 }
 
 # set up vagrant user's .bashrc
-file { '/home/vagrant/.bashrc':
-  ensure  => present,
-  content => file('/tmp/vagrant-puppet/manifests/vagrant_bashrc'),
-}
+    file { '/home/vagrant/sandbox.sh':
+      ensure  => present,
+      mode    => 0755,
+      content => file('/tmp/vagrant-puppet-2/manifests/vagrant_bashrc'),
+    }
